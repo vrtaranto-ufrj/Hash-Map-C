381c16d20a4c1698d4da4af39fdcfb8b9811d674
Vitor <vrtaranto@poli.ufrj.br>
2026-02-21 12:50:26 -0300

    Change to FNV-1a hash

--- main.c
+++ main.c
@@ -3,13 +3,13 @@
 #include "hashmap.h"
 
 int main(int argc, char const *argv[]) {
-    printf("Olá, mundo!\n");
+    HashMap hashmap = create_hashmap(16);
+    add_hashmap(&hashmap, "10", 1);
+    add_hashmap(&hashmap, "133", 19);
 
-    HashMap hashmap = create_hashmap(1);
-    add_hashmap(&hashmap, 10, 1);
-    add_hashmap(&hashmap, 133, 19);
+    HashMapReturn a = get_hashmap(&hashmap, "11");
 
-    HashMapReturn a = get_hashmap(&hashmap, 10);
+    uint64_t b = hash_func("Ola", 16);
 
     return 0;
 }
